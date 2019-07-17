1d0f0b3bf44d0a72b6972ba817dfc4e0f2f7bf8e
Jeff.Liu <jeff.liu.guo@gmail.com>
2019-07-17 14:55:41 +0800

    初始化代码

---
+++ logrus_kafkahook.go
@@ -0,0 +1,61 @@
+package logrus_kafkahook
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
+)
+
+// KafkaHook is a hook to handle writing to kafka log files.
+type KafkaHook struct {
+	formatter logrus.Formatter
+	sync      bool
+	topic     string
+	client    sarama.Client
+	levels    []logrus.Level
+}
+
+// NewHook returns new Kafka hook.
+func NewHook(client sarama.Client, topic string, formatter logrus.Formatter) *KafkaHook {
+	hook := &KafkaHook{
+		client:    client,
+		sync:      true,
+		topic:     topic,
+		formatter: formatter,
+	}
+
+	return hook
+}
+
+// Fire writes the log file to defined path or using the defined writer.
+// User who run this function needs write permissions to the file or directory if the file does not yet exist.
+func (hook *KafkaHook) Fire(entry *logrus.Entry) error {
+
+	content := hook.createContent(entry)
+
+	msg := &sarama.ProducerMessage{
+		Topic: hook.topic,
+		Key:   sarama.StringEncoder("go_log"),
+	}
+	msg.Value = sarama.ByteEncoder(content)
+	if hook.sync {
+		producer, _ := sarama.NewAsyncProducerFromClient(hook.client)
+		defer producer.AsyncClose()
+	}
+	return nil
+}
+
+func (hook *KafkaHook)createContent(entry *logrus.Entry) []byte {
+	// use our formatter instead of entry.String()
+	msg, err := hook.formatter.Format(entry)
+	if err != nil {
+		fmt.Printf("hook.formatter.Format error:%s", err.Error())
+		return []byte("")
+	}
+	return msg
+}
+
+// Levels returns configured log levels.
+func (hook *KafkaHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
---
+++ logrus_kafkahook_test.go
@@ -0,0 +1,36 @@
+package logrus_kafkahook
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+// Tests that writing to a tempfile log works.
+// Matches the 'msg' of the output and deletes the tempfile.
+func TestLogEntryWritten(t *testing.T) {
+	log := logrus.New()
+
+	config := sarama.NewConfig()
+	//等待服务器所有副本都保存成功后的响应
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	//随机向partition发送消息
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	//是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	//设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
+	//注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
+	config.Version = sarama.V0_10_0_1
+
+
+	client,err := sarama.NewClient([]string{"10.23.30.3:9092"}, config)
+	if err != nil {
+		fmt.Printf("sarama.NewClient error:%s\n", err.Error())
+	}
+	formatter := &logrus.JSONFormatter{}
+
+	hook := NewHook(client, "go_kafka_log",formatter)
+	log.Hooks.Add(hook)
+}
